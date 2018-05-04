b279f33edf26020ffbb4ae691c16caf135359277
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-04 10:56:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {Cent_30:'Gangsta',Kefha:'Wobbly',Duchess_Gogo:'Bridge_Face'};
 
 function updatePlaylist(playlist,artname,songtitle) {
-  playlist.artname = songtitle
+  playlist[artname] = songtitle
   // we could not use playlist.artname = songtitle
   return playlist
 }
